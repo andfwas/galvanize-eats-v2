b2a13f138f86bdc01b0a31dd9f073944a59ff85e
andfwas <andfwas@gmail.com>
2017-10-03 19:31:19 -0600

    separated functions BIG TIME first time

--- public/main.js
+++ public/main.js
@@ -5,9 +5,9 @@ var foods = {}
 var burgers = document.getElementById('burgers')
 var pizza = document.getElementById('pizza')
 var items = document.getElementsByClassName('item')
-var select
-var myList = document.getElementById('orders')
+
 var list = []
+var addItem = document.getElementsByClassName('addItem')
 
 
 fetch(menuItems)
@@ -16,58 +16,75 @@ fetch(menuItems)
 })
 .then(function(food) {
   foods = food.menu
+  console.log(foods)
   makeMenu(foods)
 })
 
 function makeMenu(x) {
   for (var i = 0; i < x.length; i++) {
-    var item = document.createElement('div')
-    var name = document.createElement('div')
-    var price = document.createElement('div')
-    if (x[i].type == 'burger') {
-      item.setAttribute('class', 'item inactive')
-      burgers.appendChild(item)
-      name.innerHTML = x[i].name
-      price.innerHTML = x[i].price
-      item.appendChild(name)
-      item.appendChild(price)
-      item.addEventListener('click', function() {
-        //START HERE
-        console.log(this)
-        list.push(this.innerHTML)
-        console.log(list);
-        myList.innerHTML = list
-
-        for (var y = 0; y < items.length; y++) {
-          items[y].setAttribute('class', 'item inactive')
-        }
-        this.setAttribute('class', 'item active')
-      })
-    } else {
-      item.setAttribute('class', 'item inactive')
-      pizza.appendChild(item)
-      name.innerHTML = x[i].name
-      price.innerHTML = x[i].price
-      item.appendChild(name)
-      item.appendChild(price)
-      item.addEventListener('click', function() {
-        //START HERE
-        console.log(this)
-        list.push(this.innerHTML)
-        console.log(list);
-        myList.innerHTML = list
-
-        for (var y = 0; y < items.length; y++) {
-          items[y].setAttribute('class', 'item inactive')
-        }
-        this.setAttribute('class', 'item active')
-      })
-    }
+    var j = x[i]
+    makeItem(j)
   }
 }
 
-function makeList(arr) {
-
+function makeItem(x) {
+  var item = document.createElement('div')
+  if (items.length == 0) {
+    item.setAttribute('class', 'item active')
+  } else {
+    item.setAttribute('class', 'item inactive')
+  }
+  var name = document.createElement('div')
+  var price = document.createElement('div')
+  item.appendChild(name)
+  if (x.type == 'burger') {
+    burgers.appendChild(item)
+  } else {
+    pizza.appendChild(item)
+  }
+  item.appendChild(name)
+  item.appendChild(price)
+  name.innerHTML = x.name
+  price.innerHTML = x.price
+  selectItem(item)
 }
 
-makeList(list)
+function selectItem(x) {
+  x.addEventListener('click', function() {
+    if (items) {
+      for (var i = 0; i < items.length; i++) {
+        items[i].setAttribute('class', 'item inactive')
+      }
+    }
+    x.setAttribute('class', 'item active')
+  })
+}
+
+var itemList = document.getElementById('addToList')
+
+function populateOrder() {
+  addItem[0].addEventListener('click', function() {
+    for (var i = 0; i < items.length; i++) {
+      var basketItem = document.createElement('div')
+      basketItem.setAttribute('class', 'basket')
+      var basketItemName = document.createElement('div')
+      var basketItemPrice = document.createElement('div')
+      if (items[i].className == 'item active') {
+        var name = items[i].childNodes[0].innerText
+        var price = items[i].childNodes[1].innerText
+        var ordered = document.getElementById('addToList')
+        basketItemName.innerText = name
+        basketItemPrice.innerText = price
+        basketItem.append(basketItemName)
+        basketItem.append(basketItemPrice)
+        itemList.append(basketItem)
+        // ordered.appendChild(name)
+        // ordered.appendChild(price)
+        console.log(name);
+        console.log(price);
+        console.log(ordered);
+      }
+    }
+  })
+}
+populateOrder()
